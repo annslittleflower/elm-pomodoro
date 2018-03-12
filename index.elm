9fc90dfb2de2787125ed4ac7ccb70fc331810128
sasha.bashliy <sashkacrab@gmail.com>
2018-03-12 19:07:44 +0200

    almost

--- index.elm
+++ index.elm
@@ -5,7 +5,7 @@ import Html.Events exposing (onClick)
 import Time exposing (Time, second)
 
 
-type alias Model = { 
+type alias Model = {
   text: String,
   timeToRest: Int,
   countOfPomodoro: Int,
@@ -20,7 +20,7 @@ type alias Model = {
 
 type Msg
     = Expand
-    | Disable | NoOp | Tick Time 
+    | Disable | NoOp | Tick Time
 
 init : ( Model, Cmd Msg )
 init = ( {
@@ -34,14 +34,14 @@ init = ( {
   timerStarted = False,
   isWorking = False,
   restingTimerCount = 0
-  } 
-  , 
+  }
+  ,
   Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    if model.isButtonDisabled == False then
+    if model.isButtonDisabled == False || model.isRestingNow == True then
         div []
             [
               button [ onClick Disable ] [ text "Start work" ],
@@ -49,9 +49,9 @@ view model =
             ]
     else
         div []
-            [ 
-              button [ onClick Expand, disabled model.isButtonDisabled ] [ text "Working..." ],
-              p [] [text ("Time left to work: " 
+            [
+              button [ onClick Expand, disabled (model.isButtonDisabled == True || model.isRestingNow == True) ] [ text "Working..." ],
+              p [] [text ("Time left to work: "
                 ++ (toString (model.timeToWork - model.timerCount)) ++ " --- "
                 ++ (toString (model.countOfPomodoro)) ++ " --- "
                 ++ (toString (model.timerStarted)))
@@ -64,76 +64,80 @@ update msg model =
     case msg of
         NoOp ->
             ( model, Cmd.none )
-  
+
         Expand ->
             ( model, Cmd.none )
         Disable ->
             ( {model | isButtonDisabled = True, isWorking = True, isRestingNow = False, timerCount = 0, timerStarted = True, timeToRest = 10}, Cmd.none )
         Tick newTime ->
-            
-            
-              let countOfPomodoroNew = if model.timeToWork - model.timerCount == 0 
-              then
-              model.countOfPomodoro + 1 else model.countOfPomodoro
+
+            let isRestingNow = model.isRestingNow
             in
-            
-           
-        
-            
-            let isRestingNow = if (countOfPomodoroNew % 4 == 0) 
+
+
+
+            let incrementedTimerWorking = if(isRestingNow == False && model.timeToWork - model.timerCount > 0) then model.timerCount + 1 else 0
+            in
+            let incrementedRestingTimer = if(isRestingNow == True && model.timeToRest - model.restingTimerCount >= 0) then model.restingTimerCount + 1 else 0
+            in
+
+
+
+            let countOfPomodoroNew = if model.timeToWork - model.timerCount == 0
+            then
+            model.countOfPomodoro + 1 else model.countOfPomodoro
+            in
+
+
+
+            let isRestingNowNew = if ((model.countOfPomodoro) % 4 == 0 && model.timeToWork - model.timerCount == 0)
                     then
-                      True 
+                      True
                     else False
             in
-        
-            let restingTimerCount = if (model.isRestingNow == True) then model.timeToRest - 1 else 10
-            in
-            
-             let shouldStopTimer = if (model.timeToRest - restingTimerCount == 0 && isRestingNow == True) then False else True
-            in
 
-            
-            let isStopWorking = if (model.timeToWork - model.timerCount == 0 ) then False else True
-            in
-            
-            let newTimerCount = if isRestingNow == True then 0 else model.timerCount + 1 
-            
-            in
-            
-         
-            let willButtonBeDisabled = if isStopWorking == True then
+
+
+             let shouldStopTimer = if (model.timeToRest - model.restingTimerCount <= 1 && isRestingNow == True || model.timeToWork - model.timerCount == 0) then False else True
+             in
+
+
+             let isStopWorking = if (model.timeToWork - model.timerCount == 0 ) then False else True
+             in
+
+
+
+
+            let willButtonBeDisabled = if isStopWorking == True || isRestingNowNew == True then
                   True else False
             in
-            
+
             ({
               model
-                | timerCount = newTimerCount ,
+                | timerCount = incrementedTimerWorking ,
                   countOfPomodoro = countOfPomodoroNew,
                   isButtonDisabled = willButtonBeDisabled,
-                  text = 
-                  if isRestingNow == True
-                  then 
-                  "Rest now for " ++ toString(model.timeToRest) ++ " seconds"
+                  text =
+                  if isRestingNowNew == True && (model.timeToRest - model.restingTimerCount /= 1)
+                  then
+                  "Rest now for " ++ toString(model.timeToRest - model.restingTimerCount) ++ " seconds"
                   else "Please press Start Work when you ready",
-                  isRestingNow = isRestingNow,
+                  isRestingNow = isRestingNowNew,
                   timerStarted = shouldStopTimer,
                   isWorking = isStopWorking,
-                  restingTimerCount = restingTimerCount
-                  
-                  
-        
-                  
+                  restingTimerCount = incrementedRestingTimer
+
             }, Cmd.none )
-         
-                    
-           
+
+
+
 
 subscriptions : Model -> Sub Msg
-subscriptions model = 
-    if model.isWorking == True || model.isRestingNow == True
+subscriptions model =
+    if model.timerStarted == True
     then Time.every second Tick else Sub.none
-  
-    
+
+
 
 main : Program Never Model Msg
 main =
